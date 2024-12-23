ac4682f5fb87c1d685771a7d629b581d577e267d
karol-96 <karolbhandari666@gmail.com>
2024-12-24 00:21:06 +0545

    Using Stable Diffusion in Langchain

--- LangChain/llm_chat_models.py
+++ LangChain/llm_chat_models.py
@@ -141,3 +141,87 @@ question = 'Who won the last world cup'
 summary = runnable.invoke({'question':question})
 print(summary)
 
+
+
+######### Text TO IMAGE
+from langchain.chains import LLMChain
+from langchain.prompts import PromptTemplate
+from langchain.community.utilities.dalle_image_generator import DallEAPIWrapper
+from langchain_openai import OpenAI
+import requests
+
+llm = OpenAI(temprature=0.8)
+prompt = PromptTemplate(
+    input_variables=['image_desc'],
+    template = (
+        "Generate a concise prompt to generate image based on following description.",
+        "{image_desc}"
+    )
+)
+
+chain = LLMChain(llm=llm, prompt=prompt)
+
+image_url = DallEAPIWrapper().run(chain.run("Halloween night at haunted mueseum"))
+
+
+response = requests.get(image_url)
+image_path = "haunted_house.png"
+with open(image_path, "wb") as f:
+    f.write(response.content)
+
+
+from langchain_community.llms import Replicate
+text2img = Replicate(
+       model=(
+        "stability-ai/stable-diffusion:"
+        "27b93a2413e7f36cd83da926f3656280b2931564ff050bf9575f1fdf9bcd7478"
+    ),
+    model_kwargs={'image_dimensions':'512x512'}
+)
+
+image_url = text2img('a book cover of book on creating genai using python.')
+
+
+
+###################
+from langchain_core.messages import HumanMessage
+from langchain_openai import OpenAI
+
+chat = ChatOpenAI(
+    model = "gpt-4-turbo",
+    max_tokens = 256
+)
+
+chat.invoke([
+    HumanMessage(
+        content=[
+            {"type": "text", "text": "What is this image showing"}, 
+            {
+                "type": "image_url", 
+                "image_url": { "url": image_url, "detail": "auto", },
+            }, 
+        ]
+    )
+])
+
+langchain_image = """
+The image appears to be a diagram representing the architecture or components of a software system or framework related to language processing, possibly named LangChain or associated with a project or product called LangChain, based on the prominent appearance of that term. The diagram is organized into several layers or aspects, each containing various elements or modules:\n\n1. **Protocol**: This may be the foundational layer, which includes "LCEL" and terms like parallelization, fallbacks, tracing, batching, streaming, async, and composition. These seem related to communication and execution protocols for the system.\n\n2. **Integrations Components**: This layer includes "Model I/O" with elements such as the model, output parser, prompt, and example selector. It also has a "Retrieval" section with a document loader, retriever, embedding model, vector store, and text splitter. Lastly, there\'s an "Agent Tooling" section. These components likely deal with the interaction with external data, models, and tools.\n\n3. **Application**: The application layer features "LangChain" with chains, agents, agent executors, and common application logic. This suggests that the system uses a modular approach with chains and agents to process language tasks.\n\n4. **Deployment**: This contains "Lang'"
+"""
+
+prompt = PromptTemplate(
+    input_variables = ['image_desc'],
+    template = (
+                "Simplify this image description into a concise prompt to generate an image: "
+        "{image_desc}"
+    )
+)
+
+chain = LLMChain(llm=llm,prompt = prompt)
+
+prompt = chain.run(langchain_image)
+print(prompt)
+
+image_url = DallEAPIWrapper().run(prompt)
+
+from IPython.display import Image, display
+display(Image(url=image_url))
